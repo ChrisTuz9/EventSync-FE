ebd41407b4dc804adb236c5df0c8a98c0b8efe2d
Kšyštof Tužik <krzysztof2003t@gmail.com>
2025-06-14 16:09:18 +0300

    feat: add form heading and submit button to EventForm

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -1,8 +1,10 @@
+import { UIButton } from "./uikit/UIButton";
 import { UILabeledInput } from "./uikit/UILabeledInput";
 
 export function EventForm() {
     return (
         <form className="max-w-md mx-auto p-4 m-4 shadow-md">
+            <h1 className="text-2xl font-semibold text-teal-600 mb-2">Create new event</h1>
             <UILabeledInput
                 label={"Event title:"}
                 type="text"
@@ -13,6 +15,9 @@ export function EventForm() {
                 as="textarea"
                 rows={4}
             />
+            <UIButton size="md" variant="primary">
+                Submit
+            </UIButton>
         </form>
     )
 }
